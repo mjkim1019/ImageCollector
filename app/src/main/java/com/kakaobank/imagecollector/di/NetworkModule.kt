7d2e8acdf93dec7f9617a4d10d6d5b7368e681ea
Minju Kim <mindori1019@gmail.com>
2023-03-22 15:39:21 +0900

    feature/api: networkModule hilt 작업 & api 받을 수 있도록 작업

--- app/src/main/java/com/kakaobank/imagecollector/di/NetworkModule.kt
+++ app/src/main/java/com/kakaobank/imagecollector/di/NetworkModule.kt
@@ -1,7 +1,92 @@
 package com.kakaobank.imagecollector.di
 
+import com.kakaobank.imagecollector.BuildConfig
+import com.kakaobank.imagecollector.data.ApiService
+import com.kakaobank.imagecollector.data.RemoteDataSourceImpl
+import com.kakaobank.imagecollector.util.ImageCollectorConst
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.Dispatchers
+import okhttp3.*
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 class NetworkModule {
     private val BASE_URL = "dapi.kakao.com"
     private val API_KEY = "d77fdda2c3d66fa578ffffb00f107bda"
     private val AUTH_HEADER = "KakaoAK ${API_KEY}"
+
+    private val networkInterceptor: Interceptor = Interceptor { chain ->
+        val request = chain.request()
+        try {
+            val newRequest = request.newBuilder()
+                .addHeader(ImageCollectorConst.AUTH_HEADER_KEY, AUTH_HEADER)
+                .build()
+            chain.proceed(newRequest)
+        } catch (e: Exception) {
+            Response.Builder()
+                .request(request)
+                .protocol(Protocol.HTTP_1_1)
+                .code(ImageCollectorConst.NETWORK_ERROR)
+                .body(ResponseBody.create(null, e.message ?: ""))
+                .build()
+        }
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpBuilder(): OkHttpClient.Builder {
+        val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return if (BuildConfig.DEBUG) {
+            OkHttpClient.Builder()
+                .addInterceptor(networkInterceptor)
+                .addNetworkInterceptor(httpLoggingInterceptor)
+                .connectTimeout(5, TimeUnit.SECONDS)
+                .readTimeout(5, TimeUnit.SECONDS)
+                .writeTimeout(5, TimeUnit.SECONDS)
+        } else {
+            OkHttpClient.Builder()
+                .addInterceptor(networkInterceptor)
+                .connectTimeout(5, TimeUnit.SECONDS)
+                .readTimeout(5, TimeUnit.SECONDS)
+                .writeTimeout(5, TimeUnit.SECONDS)
+        }
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClientBuilder: OkHttpClient.Builder): Retrofit {
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClientBuilder.build())
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideRemoteDataSource(apiService: ApiService): RemoteDataSourceImpl {
+        return RemoteDataSourceImpl(apiService, Dispatchers.IO)
+    }
 }
---
+++ app/src/main/java/com/kakaobank/imagecollector/util/ImageCollectorConst.kt
@@ -0,0 +1,9 @@
+package com.kakaobank.imagecollector.util
+
+object ImageCollectorConst {
+    /**
+     * NETWORK
+     */
+    const val AUTH_HEADER_KEY = "Authorization"
+    const val NETWORK_ERROR = 1001
+}
