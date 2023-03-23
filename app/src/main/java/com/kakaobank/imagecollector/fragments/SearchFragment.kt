1edd51519087262caa7dd976dcd223c92474c224
Minju Kim <mindori1019@gmail.com>
2023-03-24 01:42:25 +0900

    feature/search: SearchFragment에 adapter 연결

--- app/src/main/java/com/kakaobank/imagecollector/fragments/SearchFragment.kt
+++ app/src/main/java/com/kakaobank/imagecollector/fragments/SearchFragment.kt
@@ -1,20 +1,36 @@
 package com.kakaobank.imagecollector.fragments
 
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.kakaobank.imagecollector.R
+import com.kakaobank.imagecollector.adapters.ItemAdapter
 import com.kakaobank.imagecollector.base.BaseFragment
 import com.kakaobank.imagecollector.databinding.FragmentSearchBinding
 
-class SearchFragment: BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
     override fun createView(binding: FragmentSearchBinding) {
     }
 
     override fun viewCreated() {
+        setAdapter()
+        setListener()
+    }
+
+    private fun setAdapter() {
+        val itemAdapter = ItemAdapter {
+            // todo click 했을 때 어떻게 할지
+        }
+        binding.rvItemList.apply {
+            adapter = itemAdapter
+            layoutManager = GridLayoutManager(this.context, 2)
+        }
+    }
+
+    private fun setListener() {
         binding.btnStorage.setOnClickListener {
             val navController = findNavController()
             navController.navigate(R.id.action_searchFragment_to_storageFragment)
         }
     }
-
 
 }
