615c188e6347d0ad40f227dad24a8d88e27c9e5e
Minju Kim <mindori1019@gmail.com>
2023-03-24 01:05:33 +0900

    feature/item: Item data class 생성 & adpater 생성

---
+++ app/src/main/java/com/kakaobank/imagecollector/ui/ItemAdapter.kt
@@ -0,0 +1,54 @@
+package com.kakaobank.imagecollector.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.kakaobank.imagecollector.databinding.ItemItemBinding
+import com.kakaobank.imagecollector.model.Item
+
+class ItemAdapter(private val onClick: () -> Unit) : ListAdapter<Item, ItemAdapter.ViewHolder>(
+    ITEM_DIFF_CALLBACK
+) {
+    inner class ViewHolder(private val binding: ItemItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(image: Item) {
+            binding.apply {
+                img = image.thumbnail
+                date = image.date
+                time = image.time
+                isFavorite = image.isFavorite
+            }
+        }
+
+        init {
+            binding.btnFavorite.setOnClickListener {
+                //todo 이미지 수집
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemItemBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+
+    companion object {
+        private val ITEM_DIFF_CALLBACK = object : DiffUtil.ItemCallback<Item>() {
+            override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean =
+                oldItem == newItem
+
+            override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean =
+                oldItem.thumbnail == newItem.thumbnail
+        }
+    }
+}
