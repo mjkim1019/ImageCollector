3f7a93ec564f9829ed91aa3e358a98526b73745c
Minju Kim <mindori1019@gmail.com>
2023-03-26 16:12:56 +0900

    feature/date: dateformatter pattern으로 날짜 시간 구할 수 있게 함수 구현 & 기존 함수 localTime을 반환 타입 변경

--- app/src/main/java/com/kakaobank/imagecollector/util/DateFormatter.kt
+++ app/src/main/java/com/kakaobank/imagecollector/util/DateFormatter.kt
@@ -1,13 +1,25 @@
 package com.kakaobank.imagecollector.util
 
+import java.time.LocalDateTime
 import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
 
 object DateFormatter {
-    fun convertToItemDateAndTime(date: String): Array<String> {
-        val parseDate: OffsetDateTime = OffsetDateTime.parse(date, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
-        val outputDate = "${parseDate.year}년 ${parseDate.month.value}월 ${parseDate.dayOfMonth}일"
-        val outputTime = "${parseDate.hour}시 ${parseDate.minute}분"
-        return arrayOf(outputDate, outputTime)
+    private const val itemDatePattern = "yyyy년 MM월 dd일"
+    private const val itemTimePattern = "hh:mm a"
+
+    fun convertToItemDateAndTime(date: String): LocalDateTime {
+        val parseDate: OffsetDateTime =
+            OffsetDateTime.parse(date, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+
+        return parseDate.toLocalDateTime()
+    }
+
+    fun convertLocalDateTimeToItemDateTime(localDateTime: LocalDateTime): Array<String> {
+        val dateFormatter = DateTimeFormatter.ofPattern(itemDatePattern)
+        val timeFormatter = DateTimeFormatter.ofPattern(itemTimePattern)
+        val date = dateFormatter.format(localDateTime)
+        val time = timeFormatter.format(localDateTime)
+        return arrayOf(date, time)
     }
 }
